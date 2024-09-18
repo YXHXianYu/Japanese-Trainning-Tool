f22249a3a72298333008246f1a382c4a9ccc47ea
YXHXianYu <2943003@qq.com>
2024-09-18 19:07:45 +0800

    done

---
+++ src/main.rs
@@ -0,0 +1,111 @@
+use bevy::prelude::*;
+
+#[allow(dead_code)]
+const LIB_5: [(&str, &str); 5] = [
+    ("あ", "a"), ("い", "i"), ("う", "u"), ("え", "e"), ("お", "o")
+];
+
+#[allow(dead_code)]
+const LIB_50: [(&str, &str); 46] = [
+    ("あ", "a"), ("い", "i"), ("う", "u"), ("え", "e"), ("お", "o"),
+    ("か", "ka"), ("き", "ki"), ("く", "ku"), ("け", "ke"), ("こ", "ko"),
+    ("さ", "sa"), ("し", "shi"), ("す", "su"), ("せ", "se"), ("そ", "so"),
+    ("た", "ta"), ("ち", "chi"), ("つ", "tsu"), ("て", "te"), ("と", "to"),
+    ("な", "na"), ("に", "ni"), ("ぬ", "nu"), ("ね", "ne"), ("の", "no"),
+    ("は", "ha"), ("ひ", "hi"), ("ふ", "fu"), ("へ", "he"), ("ほ", "ho"),
+    ("ま", "ma"), ("み", "mi"), ("む", "mu"), ("め", "me"), ("も", "mo"),
+    ("や", "ya"), ("ゆ", "yu"), ("よ", "yo"),
+    ("ら", "ra"), ("り", "ri"), ("る", "ru"), ("れ", "re"), ("ろ", "ro"),
+    ("わ", "wa"), ("を", "wo"), ("ん", "n"),
+];
+
+#[allow(dead_code)]
+const LIB_71: [(&str, &str); 71] = [
+    ("あ", "a"), ("い", "i"), ("う", "u"), ("え", "e"), ("お", "o"),
+    ("か", "ka"), ("き", "ki"), ("く", "ku"), ("け", "ke"), ("こ", "ko"),
+    ("さ", "sa"), ("し", "shi"), ("す", "su"), ("せ", "se"), ("そ", "so"),
+    ("た", "ta"), ("ち", "chi"), ("つ", "tsu"), ("て", "te"), ("と", "to"),
+    ("な", "na"), ("に", "ni"), ("ぬ", "nu"), ("ね", "ne"), ("の", "no"),
+    ("は", "ha"), ("ひ", "hi"), ("ふ", "fu"), ("へ", "he"), ("ほ", "ho"),
+    ("ま", "ma"), ("み", "mi"), ("む", "mu"), ("め", "me"), ("も", "mo"),
+    ("や", "ya"), ("ゆ", "yu"), ("よ", "yo"),
+    ("ら", "ra"), ("り", "ri"), ("る", "ru"), ("れ", "re"), ("ろ", "ro"),
+    ("わ", "wa"), ("を", "wo"), ("ん", "n"),
+    ("が", "ga"), ("ぎ", "gi"), ("ぐ", "gu"), ("げ", "ge"), ("ご", "go"),
+    ("ざ", "za"), ("じ", "ji"), ("ず", "zu"), ("ぜ", "ze"), ("ぞ", "zo"),
+    ("だ", "da"), ("ぢ", "ji"), ("づ", "zu"), ("で", "de"), ("ど", "do"),
+    ("ば", "ba"), ("び", "bi"), ("ぶ", "bu"), ("べ", "be"), ("ぼ", "bo"),
+    ("ぱ", "pa"), ("ぴ", "pi"), ("ぷ", "pu"), ("ぺ", "pe"), ("ぽ", "po"),
+];
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, update)
+        .run();
+}
+
+#[derive(Component, Debug)]
+struct Data(i32);
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut windows: Query<&mut Window>,
+) {
+    // window
+    let mut window = windows.single_mut();
+    window.resolution.set(400.0, 300.0);
+
+    // camera
+    commands.spawn(Camera2dBundle::default());
+
+    // text
+    let font = asset_server.load("NotoSansJP-Regular.ttf");
+    let text_style = TextStyle {
+        font: font.clone(),
+        font_size: 100.0,
+        ..Default::default()
+    };
+    let text_justification = JustifyText::Center;
+
+    commands.spawn((
+        Text2dBundle {
+            text: Text::from_section("Press Space", text_style.clone())
+                .with_justify(text_justification),
+            ..Default::default()
+        },
+        Data(-1),
+    ));
+}
+
+fn update(
+    kb: Res<ButtonInput<KeyCode>>,
+    mut x: Query<(&mut Text, &mut Data)>,
+) {
+    if kb.just_pressed(KeyCode::Space) != true {
+        return;
+    }
+
+    // MARK: - Choose the library
+    // ========================================
+    // ========================================
+    let lib_chosen = LIB_5;
+    // ========================================
+    // ========================================
+
+    let mut y = x.iter_mut().next().unwrap();
+
+    let char = if y.1.0 == -1 {
+        let v = rand::random::<usize>() % lib_chosen.len();
+        y.1.0 = v as i32;
+        lib_chosen[v as usize].0
+    } else {
+        let v = y.1.0;
+        y.1.0 = -1;
+        lib_chosen[v as usize].1
+    };
+
+    y.0.sections[0].value = char.to_string();
+}
